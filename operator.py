2ff1a90fab3f49a84459d0b31957daf0503155a1
mumpydas-ux <mumpy.das@gmail.com>
2026-08-16 12:38:55 -0400

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,29 @@
+total = 120 + 85 + 150 + 95 + 110 # addition
+
+earnings = total * 15 # multiplication
+
+average = total / 5 # division (float)
+
+print(total, earnings, average)
+
+total   = 500
+bag_size = 25
+bags     = total // bag_size
+leftover = total % bag_size
+print("full bags:", bags)
+print("leftover:", leftover) 
+
+total   = 560
+last_year = 500
+print(total > last_year)
+print(total < 400) 
+print(total == last_year)
+print(total >= last_year)
+
+total = 560
+total += 30
+print(total)
+total -= 15
+print(total)
+bags = total // 25
+print(bags)
