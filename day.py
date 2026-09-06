930f8622942887cd5103c0f74aa1d61f96c92870
mumpydas-ux <mumpy.das@gmail.com>
2026-09-06 13:40:45 -0300

    Add files via upload

---
+++ day.py
@@ -0,0 +1,30 @@
+day = input("Enter the day of the week: ")
+
+if day in ("Saturday", "Sunday"):
+    print("day type :  Enjoy free time!")
+elif day == "Monday":
+    print("day type :  first day of the week.!")
+elif day == "Friday":
+    print("day type : last school day .!")
+elif day in ("Tuesday", "Wednesday", "Thursday"):
+    print("day type :  regular school day .!")
+else:
+    print("day type :  day not recognized.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
