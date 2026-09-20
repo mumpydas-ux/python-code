9ef388e35ca3aa2f5d295bf8fb9915e9c1a74f3e
mumpydas-ux <mumpy.das@gmail.com>
2026-09-20 13:31:39 -0300

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+mean1 = 38
+wrong_number =  36
+correct_number = 56
+total_number = 40
+sum = mean1 * total_number
+num2 = sum - ((wrong_number) - (correct_number))
+mean2 = num2 / total_number
+print(mean2)
