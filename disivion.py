a096077c69a0c8a782d08f9c5b276c55ab167e74
mumpydas-ux <mumpy.das@gmail.com>
2026-09-20 13:20:15 -0300

    Add files via upload

---
+++ disivion.py
@@ -0,0 +1,6 @@
+numn = int(input())
+numd = int(input())
+if numn % numd == 0:
+    print(str(numn) + " is divisible by " + str(numd))
+else:
+    print(str(numn) + " is not divisible by " + str(numd))
